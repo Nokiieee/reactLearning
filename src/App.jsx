5f367dbb88cf092cd6f9eedf4f4a1923ce736a9a
Noki <denenoch.mendoza@gmail.com>
2026-04-23 22:16:29 +0800

    work with POST & DELETE request and also with ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,24 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
-  const addJob = (newJob) => {
-    console.log(newJob);
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
   };
 
   const router = createBrowserRouter(
@@ -22,7 +38,11 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>,
     ),
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <>
-        <Navbar />
-        <Outlet />
+      <Navbar />
+      <Outlet />
+      <ToastContainer />
+      {/* you can put <ToastContainer /> anywhere. It's positioned absolute */}
     </>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
